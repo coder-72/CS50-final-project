2f5baad8f6b7c9c847c85261ed58be153789302d
coder-72 <122729719+coder-72@users.noreply.github.com>
2024-10-13 09:21:47 +0000

    commit with posts working

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 @app.route("/post/<int:post_id>")
 def posts(post_id:int):
     dict = utils.get_post(int(post_id))
-    return render_template("views/post.html", page_title="blog-post", title=dict["title"], subheading=dict["subtitle"], date=utils.format_date(dict["date"]), image=dict["image"], post_content=dict["content"])
+    return render_template("views/post.html", page_title="blog-post", title=dict["title"], subheading=dict["subtitle"], date=utils.format_date(dict["date"]), image=dict["image"], post_content=utils.markdown_to_html(dict["content"]))
 
 
 app.run(debug=True)
