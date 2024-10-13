73970294b30b813bee904dc22da0fa30464f79df
coder-72 <122729719+coder-72@users.noreply.github.com>
2024-10-13 08:46:17 +0000

    commit

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import render_template, Flask, request
 import utils
-import markdown2
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("views/index.html", page_title ="title", )
+    return render_template("views/index.html", page_title="blog-home page", )
 
 @app.route("/post/<int:post_id>")
 def posts(post_id:int):
     dict = utils.get_post(int(post_id))
-    return render_template("")
+    return render_template("views/post.html", page_title="blog-post", title=dict["title"], subheading=dict["subtitle"], date=utils.format_date(dict["date"]), image=dict["image"], post_content=dict["content"])
 
 
 app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,14 +1,27 @@
 import sqlite3 
+from datetime import datetime
+import markdown2
 
-conn = sqlite3.connect("blog.db")
-cursor = conn.cursor()
+def get_post(post_id):
+    # Create a new connection for this thread
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
 
-def get_post(id:int):
-    cursor.execute("SELECT * FROM posts WHERE id = ? LIMIT 1 ORDER BY date DESC", (id))
-    result = cursor.fetchall()
-    if len(result) == 1:
-        result = result[0]
-    else:
-        raise Exception("get_post returned more than 1 post")
-    return result
+    cursor.execute("SELECT * FROM posts WHERE id = ? LIMIT 1", (post_id,))
+    post = cursor.fetchone()
+    print(post)
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    return post
+
+
     
+def format_date(date:str):
+    date_obj = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    return date_obj.strftime("%a %d %b %Y")
+
+def markdown_to_html(text:str):
